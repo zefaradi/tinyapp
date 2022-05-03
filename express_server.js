0a934b389211f12576271050b52398a839d86324
Adib Zefar <adibzefar@gmail.com>
2022-05-02 20:11:24 -0400

    Add app.get with res.render(urls_index)

--- express_server.js
+++ express_server.js
@@ -13,6 +13,11 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+})
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { 
     shortURL: req.params.shortURL, 
