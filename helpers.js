ac4ea6d349b4326d6706a18a218bae321cf8f636
Adib Zefar <adibzefar@gmail.com>
2022-05-09 23:49:16 -0400

    Create a Helper Functions Module

---
+++ helpers.js
@@ -0,0 +1,44 @@
+const { urlDatabase, users } = require('./database');
+
+//HELPER FUNCTIONS
+
+// program to generate random strings for URLs and and User IDs.
+const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+function generateRandomString() {
+    let result = ' ';
+    let length = 6;
+    const charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result.trim();
+}
+
+//Checks if an E-mail already exists in the user database
+const getUserByEmail  = (email, database) => {
+  for (let user in database) {
+    if (database[user].email === email) {
+      return database[user];
+    }
+  }
+  return undefined;
+};
+
+//Retrieves a list of urls assigned to a particular user
+const urlsForUser = (id, database) => {
+  let listURL = {};
+  for (let user in database) {
+    if (database[user].userID === id) {
+      listURL[user] = database[user];
+    }
+  }
+  return listURL;
+}
+
+module.exports = {
+  getUserByEmail,
+  generateRandomString,
+  urlsForUser,
+};
