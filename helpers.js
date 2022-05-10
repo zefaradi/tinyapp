bce90d6328b43bfb2da07b59eb8b7af9f8975e72
Adib Zefar <adibzefar@gmail.com>
2022-05-10 15:22:47 -0400

    Remove .trim() from return result

--- helpers.js
+++ helpers.js
@@ -4,14 +4,14 @@
 const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
 const generateRandomString = () => {
-  let result = ' ';
+  let result = '';
   let length = 6;
   const charactersLength = characters.length;
   for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
 
-  return result.trim();
+  return result;
 };
 
 //Checks if an E-mail already exists in the user database
